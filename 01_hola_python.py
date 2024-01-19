8180bcbe904808783e49eb06a953759dc3e98821
unknown <ismael2095951@gmail.com>
2024-01-19 17:25:24 -0300

    datos iniciales

---
+++ 01_hola_python.py
@@ -0,0 +1,30 @@
+
+
+# Esto es un comentario de una línea 
+"""
+éste es un comentario 
+de varias 
+líneas
+
+"""
+
+'''
+éste es otro comentario
+de varias
+líneas
+
+'''
+
+# imprime por consola: hola mundo
+print('hola python')  
+print("hola python")
+# si hacemos click en el punto rojo del lado izquierdo de la línea de código podemos hacer una interrupción del programa y poder ejecutar o para hacer debug (resolver problemas)
+
+# consultar el tipo de datos de una variable
+
+print(type('hola python')) # Tipo 'str' -> string
+print(type(10)) # Tipo 'int' -> número entero
+print(type(1.6)) # Tipo 'float' -> no entero
+print(type(True)) # Tipo 'bool' -> boolena
+print(type(1 + 6j)) # Tipo 'complex' -> número complejo
+print(type(print('probando que sale'))) # Tipo 'NoneType' -> no tiene Tipo ya que es una función de sistema
